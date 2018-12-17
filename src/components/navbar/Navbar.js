23e3a027d46bdb859973408273f0c5f855edc20e
Bryan Astacio <brya0196@gmail.com>
2018-12-17 19:58:45 -0400

    created the asstes and components folder

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+      <header className="navbar">
+          
+      </header>
+  );
+};
+
+export default Navbar;
