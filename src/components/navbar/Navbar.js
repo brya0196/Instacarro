d31a6f4245c46cee7d4703bc25febceb0bca631e
Bryan Astacio <brya0196@gmail.com>
2018-12-17 23:26:29 -0400

    added the navbar design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Navbar.css';
 
 import logo from "../../assets/images/logo.png";
-import phone from "../../assets/svg/phone.svg"
+import phone from "../../assets/svg/phone.svg";
+import user from "../../assets/svg/user.svg";
+import caret from "../../assets/svg/caret.svg";
 
 const Navbar = () => {
   return (
@@ -16,6 +18,16 @@ const Navbar = () => {
       <div className="phone">
         <img src={phone} />
         <span>(11) 3569-3465</span>
+      </div>
+
+      <div className="userData">
+        <div className="userInfo">
+          <img src={user} />
+          <span>USER TEST</span>
+        </div>
+        <div className="arrow">
+          <img src={caret} />
+        </div>
       </div>
     </nav>
   );
