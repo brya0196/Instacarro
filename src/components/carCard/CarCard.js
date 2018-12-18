a1d8e4cb56905d5da5aefbfd93e601c01e8e9c47
Bryan Astacio <bastacio@Bryans-MacBook-Pro.local>
2018-12-18 11:51:57 -0400

    added the structure of the card

--- src/components/carCard/CarCard.js
+++ src/components/carCard/CarCard.js
@@ -14,11 +14,41 @@ class CarCard extends Component {
 
     return (
       <div className="Card">
-      
+
         <div className="header">
           <div className="image" style={{backgroundImage: 'url('+this.props.car.imageUrl+')'}}>
             <span className="text">ver detalhes</span>
           </div>
+        </div>
+
+        <div className="body">
+          <div className="row">
+            <div className="col">
+              <span className="text">TEMPO RESTANTE</span>
+              <span className="number red">{this.props.car.remainingTime}</span>
+            </div>
+            <div className="col">
+              <span className="text">ULTIMA OFERTA</span>
+              <span className="number red">R$</span>
+            </div>
+          </div>
+          
+          <div className="row">
+            {this.props.car.make} {this.props.car.model} {this.props.car.version} {this.props.car.year}
+          </div>
+
+          <div className="row">
+            <div className="col">
+              <span className="text">{this.props.car.year}</span>
+            </div>
+            <div className="col">
+              <span className="text">{this.props.car.km} KM</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer">
+          <button type="btn" className="button button-green">FAZER OFERTA</button>
         </div>
 
       </div>
