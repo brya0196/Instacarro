c15f05655ceba36a7a58e94487d44e4f4cbc2955
Bryan Astacio <bastacio@Bryans-MacBook-Pro.local>
2018-12-18 16:53:31 -0400

    added the CalculateAmount and Miliseconds to hours function, decrease by second and make offert function

--- src/components/carCard/CarCard.js
+++ src/components/carCard/CarCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './CarCard.css';
 
 
@@ -6,9 +7,67 @@ class CarCard extends Component {
   
   constructor(props) {
     super(props);
+
+    this.state = {
+      amount: 0,
+      time: undefined
+    }
+
+    this.CalculateAmount = this.CalculateAmount.bind(this);
+    this.MilisecondsToHours = this.MilisecondsToHours.bind(this);
+    this.DecreaseBySecond = this.DecreaseBySecond.bind(this);
+    this.MakeOffert = this.MakeOffert.bind(this);
+  }
+
+  componentDidMount() {
+    this.CalculateAmount();
+    this.interval = setInterval(() => this.DecreaseBySecond(), 1000);
+    this.setState({ 
+      time: this.props.car.remainingTime
+    });
+  }
+
+  componentWillMount() {
+    clearInterval(this.interval);
+  }
+
+  CalculateAmount() {
+    let bids = this.props.car.bids;
+
+    let amount = 0;
+    bids.map((item) => {
+      amount += Number(item.amount);
+    });
+
+    this.setState({ 
+      amount: amount
+    });
+  }
+
+  MilisecondsToHours(miliseconds) {
+    let time = moment.duration(miliseconds, 'milliseconds');
+    
+    return time.hours() + ":" + time.minutes() + ":" + time.seconds();
+  }
+
+  DecreaseBySecond() {
+    let time = 0;
+
+    time = moment.duration(this.state.time) - 1000;
+
+    this.setState({ time: time });
+  }
+
+  MakeOffert() {
+    let amount = this.state.amount + 250;
+  
+    this.setState({ amount: amount });
   }
 
   render() {
+    var time = this.props.car.remainingTime;
+
+    if (this.state.time != undefined) time = this.state.time;
 
     return (
       <div className="Card">
@@ -23,12 +82,14 @@ class CarCard extends Component {
           <div className="row">
             <div className="col">
               <span className="text">TEMPO RESTANTE</span>
-              <span className="number red">{this.props.car.remainingTime}</span>
+              <span className="number red">
+                {this.MilisecondsToHours(time)}
+              </span>
             </div>
             <div className="bar-v"></div>
             <div className="col">
               <span className="text">ULTIMA OFERTA</span>
-              <span className="number green">R$</span>
+              <span className="number green">R$ {this.state.amount}</span>
             </div>
           </div>
 
@@ -59,7 +120,7 @@ class CarCard extends Component {
         
 
         <div className="footer">
-          <button type="btn" className="button button-green">FAZER OFERTA</button>
+          <button type="btn" className="button button-green" onClick={() => this.MakeOffert()}>FAZER OFERTA</button>
         </div>
 
       </div>
